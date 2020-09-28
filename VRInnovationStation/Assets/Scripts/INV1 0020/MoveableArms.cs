682b56b53354357d4f60ae65e19c2d77c21220ef
KylePCT <ktugwell@live.co.uk>
2020-09-28 16:55:13 +0100

    File structure updates and lever changes

---
+++ VRInnovationStation/Assets/Scripts/INV1 0020/MoveableArms.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableArms : MonoBehaviour
+{
+    public GameObject ToothLever;
+    public GameObject ArmHandle;
+
+    public GameObject TopCollider;
+
+    private bool ArmsCanMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ArmsCanMove = false;
+        ArmHandle.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "HiddenCollision")
+        {
+            ArmsCanMove = true;
+            MoveArms();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ArmsCanMove = false;
+        ArmHandle.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    public void MoveArms()
+    {
+        if (ArmsCanMove == true)
+        {
+            ArmHandle.GetComponent<BoxCollider>().enabled = true;
+            Debug.Log("Arms can be moved.");
+        }
+    }
+}
--- VRInnovationStation/Library/PackageCache/com.unity.xr.interaction.toolkit@0.9.4-preview/Runtime/Interaction/Interactables/XRGrabInteractable.cs
+++ VRInnovationStation/Library/PackageCache/com.unity.xr.interaction.toolkit@0.9.4-preview/Runtime/Interaction/Interactables/XRGrabInteractable.cs
@@ -439,9 +439,7 @@ namespace UnityEngine.XR.Interaction.Toolkit
             base.OnSelectExit(interactor);
 
             if (m_RetainTransformParent)
-            {
                 transform.parent = m_OriginalSceneParent;
-            }
 
             // reset RididBody settings
             m_RigidBody.isKinematic = m_WasKinematic;
