23b740061a8f4767928aa81812a5f9942c0a33f1
KylePCT <ktugwell@live.co.uk>
2020-10-13 12:53:21 +0100

    Further functionality changes

---
+++ VRInnovationStation/Assets/Animations/UI/UI_ShowIssueMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ShowIssueMessage : MonoBehaviour
+{
+    public Animator UIIssue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIIssue = GetComponent<Animator>();
+    }
+
+    public void StartIssueMessageFade()
+    {
+        UIIssue.SetBool("CanAnimate", true);
+    }
+}
---
+++ VRInnovationStation/Assets/Scripts/INV1 0020/TaskChecks/TC_FeetInPlace.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TC_FeetInPlace : MonoBehaviour
+{
+    public GameObject Col_LeftFrontArmPlace;
+    public GameObject Col_LeftBackArmPlace;
+    public GameObject Col_RightFrontArmPlace;
+    public GameObject Col_RightBackArmPlace;
+
+    private BoxCollider LeftFrontCol;
+    private BoxCollider LeftBackCol;
+    private BoxCollider RightFrontCol;
+    private BoxCollider RightBackCol;
+
+    private bool IsLeftFrontInPlace = false;
+    private bool IsLeftBackInPlace = false;
+    private bool IsRightFrontInPlace = false;
+    private bool IsRightBackInPlace = false;
+    [HideInInspector]
+    public bool AreAllFeetInPlace = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeftFrontCol = Col_LeftFrontArmPlace.GetComponent<BoxCollider>();
+        LeftBackCol = Col_LeftBackArmPlace.GetComponent<BoxCollider>();
+        RightFrontCol = Col_RightFrontArmPlace.GetComponent<BoxCollider>();
+        RightBackCol = Col_RightBackArmPlace.GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
