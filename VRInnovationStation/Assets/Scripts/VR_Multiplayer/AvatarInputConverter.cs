8ac6cd89e3247654774fca83776c3b80dff5eca5
KylePCT <ktugwell@live.co.uk>
2021-01-27 15:05:16 +0000

    Minor code edit

--- VRInnovationStation/Assets/Scripts/VR_Multiplayer/AvatarInputConverter.cs
+++ VRInnovationStation/Assets/Scripts/VR_Multiplayer/AvatarInputConverter.cs
@@ -22,8 +22,6 @@ public class AvatarInputConverter : MonoBehaviour
     public Vector3 headPositionOffset;
     public Vector3 handRotationOffset;
 
-    
-
     // Update is called once per frame
     void Update()
     {
@@ -31,7 +29,7 @@ public class AvatarInputConverter : MonoBehaviour
         MainAvatarTransform.position = Vector3.Lerp(MainAvatarTransform.position, XRHead.position + headPositionOffset, 0.5f);
         AvatarHead.position = Vector3.Lerp(AvatarHead.position, XRHead.position + headPositionOffset, 0.5f);
         AvatarHead.rotation = Quaternion.Lerp(AvatarHead.rotation, XRHead.rotation, 0.5f);
-        AvatarBody.position = new Vector3(AvatarHead.position.x, (XRHead.position.y - 1.2f), AvatarHead.position.z);
+        AvatarBody.position = new Vector3(AvatarHead.position.x, (XRHead.position.y - 1.1f), AvatarHead.position.z);
         AvatarBody.rotation = Quaternion.Lerp(AvatarBody.rotation, Quaternion.Euler(new Vector3(0, AvatarHead.rotation.eulerAngles.y, 0)), 0.05f);
 
         //Hands synch
